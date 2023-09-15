f53d31ad7e011a872e57efea0ce9f87266a9b0d5
Poonam Gaikwad <93325537+Poonam2002@users.noreply.github.com>
2023-09-15 22:53:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DeluxPizza dp =  new DeluxPizza(true);
+        dp.getBill();
+        dp.addExtraCheese();
+        dp.addExtraToppings();
+    }
+
+
+
+}
